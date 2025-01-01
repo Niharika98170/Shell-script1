03065c482b0050701da9148d350bb098ea299c12
Niharika98170 <niharikaashinaveni98@gmail.com>
2025-01-01 19:53:18 +0530

    conversation added

---
+++ 06-arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MOVIES = ("Puspha" "rrr" "devara")
+
+echo "First MOVIE: $MOVIES{[0]}"
+echo "Second MOVIE: $MOVIES{[1]}"
+echo "Third MOVIE: $MOVIES{[2]}"
+
+echo "All movies are: $MOVIES{[@]}"
